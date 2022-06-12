60933e9977a44eae04959b0f2f66da81b946ae8f
Zhi <neozhixuan@gmail.com>
2022-06-12 12:19:35 +0800

    attempt-1

--- pages/app/index.js
+++ pages/app/index.js
@@ -1,12 +1,31 @@
-import React from 'react'
-import Layout from '../../components/layout'
+import { AddIcon, CheckIcon, DeleteIcon, EditIcon } from "@chakra-ui/icons";
+import { Box, Center, Flex } from "@chakra-ui/react";
+// STEPPER IMPORTS
+import { Step, Steps } from "chakra-ui-steps";
+import dynamic from "next/dynamic";
+import { useState } from "react";
+import Head from "../../components/Head";
+import {AddItem} from "../../components/Steps/AddItem";
+import GeneralWaste from "../jsonfiles/General-Waste.json";
+import Item from "../jsonfiles/Item.json";
+
+const GeolocationNoSSR = dynamic(
+  () =>
+    import(
+      "../components/recycleAndReuseComponents/Steps/StepThree/Geolocation"
+    ),
+  {
+    loading: () => <p>Map is loading</p>,
+    ssr: false,
+  }
+);
 
 const CustomerApp = () => {
-  return <div>CustomerApp</div>
-}
+  return <div>CustomerApp</div>;
+};
 
 CustomerApp.getLayout = function getLayout(page) {
-  return <Layout>{page}</Layout>
-}
+  return <Layout>{page}</Layout>;
+};
 
-export default CustomerApp
+export default CustomerApp;
