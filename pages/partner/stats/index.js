fc37fb91e25169f4e2d05a1af7f426a655b55e98
Shen Nan <wongshennan@gmail.com>
2022-06-12 21:01:43 +0800

    fix: stat pages for fmcg and partner

--- pages/partner/stats/index.js
+++ pages/partner/stats/index.js
@@ -1,12 +1,48 @@
 import React from 'react'
-import Layout from '../../components/Layout'
+import Layout from '../../../components/Layout'
+import { SimpleGrid, Stack } from '@chakra-ui/react'
+import StatCell from '../../../components/stat/StatCell'
 
-const GrabApp = () => {
-  return <div>GrabApp</div>
+const stats = [
+  {
+    label: 'Lifetime Carbon Offsets',
+    value: '71,887 tonnes',
+    delta: { value: '320', isUpwardsTrend: true },
+  },
+  {
+    label: 'Avg. Offsets / month',
+    value: '56.87 tonnes',
+    delta: { value: '2.3%', isUpwardsTrend: true },
+  },
+  {
+    label: 'Avg. Pts Earned / month',
+    value: '12.87 pts',
+    delta: { value: '0.1%', isUpwardsTrend: true },
+  },
+]
+
+const Stats = () => {
+  return (
+    <Stack spacing={{ base: '8', lg: '6' }} maxWidth={'3xl'}>
+      <Stack
+        spacing='4'
+        direction={{ base: 'column', lg: 'row' }}
+        justify='space-between'
+        mt={10}
+      ></Stack>
+      <Stack spacing={{ base: '5', lg: '6' }}>
+        <SimpleGrid columns={{ base: 1, md: 3 }} mx='20' gap='6'>
+          {stats.map((stat, id) => (
+            <StatCell key={id} {...stat} />
+          ))}
+        </SimpleGrid>
+      </Stack>
+    </Stack>
+  )
 }
 
-GrabApp.getLayout = function getLayout(page) {
+Stats.getLayout = function getLayout(page) {
   return <Layout>{page}</Layout>
 }
 
-export default GrabApp
+export default Stats
