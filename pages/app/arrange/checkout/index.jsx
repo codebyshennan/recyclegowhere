ab0d97a529366308b6cdd77f144b73ded67a4608
Shen Nan <wongshennan@gmail.com>
2022-06-13 00:03:30 +0800

    fix: missing key

--- pages/app/arrange/checkout/index.jsx
+++ pages/app/arrange/checkout/index.jsx
@@ -7,77 +7,77 @@ import {
   Center,
   Container,
   Text,
-} from "@chakra-ui/react";
-import { useRouter } from "next/router";
-import { useState } from "react";
-import React from "react";
-import { PaymentInformation } from "../../../../components/checkout/PaymentInformation";
-import { ShippingInformation } from "../../../../components/checkout/ShippingInformation";
-import { ShippingMethod } from "../../../../components/checkout/ShippingMethod";
-import { OrderSummary } from "../../../../components/checkout/OrderSummary";
-import Layout from "../../../../components/Layout";
-import Calendar from "react-calendar";
+} from '@chakra-ui/react'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
+import React from 'react'
+import { PaymentInformation } from '../../../../components/checkout/PaymentInformation'
+import { ShippingInformation } from '../../../../components/checkout/ShippingInformation'
+import { ShippingMethod } from '../../../../components/checkout/ShippingMethod'
+import { OrderSummary } from '../../../../components/checkout/OrderSummary'
+import Layout from '../../../../components/Layout'
+import Calendar from 'react-calendar'
 
 const Checkout = () => {
-  const router = useRouter();
-  const encode = router.query;
-  console.log(encode);
-  console.log(encode.code);
-  console.log(encode.item);
-  const items = JSON.parse(atob(encode.item));
-  const [value, setValue] = useState(new Date());
+  const router = useRouter()
+  const encode = router.query
+  console.log(encode)
+  console.log(encode.code)
+  console.log(encode.item)
+  const items = JSON.parse(atob(encode.item))
+  const [value, setValue] = useState(new Date())
   return (
     <Box
-      height="100%"
+      height='100%'
       bgGradient={useColorModeValue(
-        "linear(to-l, gray.50 50%, white 50%)",
-        "linear(to-l, gray.700 50%, gray.800 50%)"
+        'linear(to-l, gray.50 50%, white 50%)',
+        'linear(to-l, gray.700 50%, gray.800 50%)'
       )}
     >
-      <Flex maxW="8xl" mx="auto" direction={{ base: "column", md: "row" }}>
+      <Flex maxW='8xl' mx='auto' direction={{ base: 'column', md: 'row' }}>
         <Box
-          flex="1"
-          bg={useColorModeValue("white", "gray.800")}
-          px={{ base: "4", md: "8", lg: "12", xl: "20" }}
+          flex='1'
+          bg={useColorModeValue('white', 'gray.800')}
+          px={{ base: '4', md: '8', lg: '12', xl: '20' }}
         >
           <Center>
-            <Stack spacing={{ base: "16", lg: "20" }} textAlign={"center"}>
+            <Stack spacing={{ base: '16', lg: '20' }} textAlign={'center'}>
               {/* <ShippingInformation />
               <ShippingMethod />
               <PaymentInformation /> */}
 
               <Box>
                 <div
-                  as="section"
-                  pt={{ base: "4", md: "8" }}
-                  pb={{ base: "12", md: "24" }}
+                  as='section'
+                  pt={{ base: '4', md: '8' }}
+                  pb={{ base: '12', md: '24' }}
                 >
                   <Container my={5}>
                     <Box
-                      bg="bg-surface"
-                      px={{ base: "4", md: "6" }}
-                      py="5"
-                      boxShadow={useColorModeValue("sm", "sm-dark")}
-                      borderTopWidth="4px"
-                      borderColor="accent"
+                      bg='bg-surface'
+                      px={{ base: '4', md: '6' }}
+                      py='5'
+                      boxShadow={useColorModeValue('sm', 'sm-dark')}
+                      borderTopWidth='4px'
+                      borderColor='accent'
                     >
-                      <Stack spacing="1">
-                        <Text fontSize="lg" fontWeight="medium">
+                      <Stack spacing='1'>
+                        <Text fontSize='lg' fontWeight='medium'>
                           Order overview
                         </Text>
-                        <Text color="muted" fontSize="sm">
+                        <Text color='muted' fontSize='sm'>
                           Summary of your delivery
                         </Text>
                       </Stack>
                     </Box>
                   </Container>
-                </div>{" "}
-                <strong>Your location:</strong> <br /> Lat: {encode.code[0]}{" "}
+                </div>{' '}
+                <strong>Your location:</strong> <br /> Lat: {encode.code[0]}{' '}
                 <br /> Long: {encode.code[1]}
                 <br />
                 <br />
                 {items.map((item, idx) => (
-                  <div>
+                  <div key={`${item.category}-${idx}`}>
                     <p>
                       <strong>Your Items:</strong>
                     </p>
@@ -98,24 +98,24 @@ const Checkout = () => {
           </Center>
         </Box>
         <Box
-          flex="1"
-          maxW={{ lg: "md", xl: "40rem" }}
+          flex='1'
+          maxW={{ lg: 'md', xl: '40rem' }}
           bg={useBreakpointValue({
-            base: useColorModeValue("white", "gray.800"),
-            md: "inherit",
+            base: useColorModeValue('white', 'gray.800'),
+            md: 'inherit',
           })}
-          px={{ base: "4", md: "8", lg: "12", xl: "20" }}
-          py={{ base: "6", md: "8", lg: "12", xl: "20" }}
+          px={{ base: '4', md: '8', lg: '12', xl: '20' }}
+          py={{ base: '6', md: '8', lg: '12', xl: '20' }}
         >
           <OrderSummary />
         </Box>
       </Flex>
     </Box>
-  );
-};
+  )
+}
 
 Checkout.getLayout = function getLayout(page) {
-  return <Layout>{page}</Layout>;
-};
+  return <Layout>{page}</Layout>
+}
 
-export default Checkout;
+export default Checkout
